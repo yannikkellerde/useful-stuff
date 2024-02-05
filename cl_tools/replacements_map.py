47ed80d848422bc45888855972023f8ca2d87928
yannikkellerde <ykeller@mpib-berlin.mpg.de>
2024-02-05 11:01:24 +0100

    recurse

--- cl_tools/replacements_map.py
+++ cl_tools/replacements_map.py
@@ -1,4 +1,5 @@
 import json, argparse
+import os
 
 
 def replacements_map():
@@ -8,18 +9,47 @@ def replacements_map():
     parser.add_argument(
         "--dry", action="store_true", help="Only print replaced file contents"
     )
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        help="Recurse through directories and replace text in all files",
+    )
+    parser.add_argument(
+        "-e",
+        "--extensions",
+        nargs="+",
+        default=[],
+        help="Only replace text in files with these extensions",
+    )
     args = parser.parse_args()
     with open(args.map, "r") as f:
         replace_map = json.load(f)
 
-    with open(args.file, "r") as f:
-        text = f.read()
-
-    for k, v in replace_map.items():
-        text = text.replace(k, v)
-
-    if args.dry:
-        print(text)
+    if os.path.isfile(args.file):
+        filelist = [args.file]
+    elif os.path.isdir(args.file):
+        assert args.recursive, "Directory provided without --recursive flag"
+        filelist = []
+        for root, _dirs, files in os.walk(args.file):
+            for file in files:
+                if args.extensions:
+                    if os.path.splitext(file)[-1] in args.extensions:
+                        filelist.append(os.path.join(root, file))
+                else:
+                    filelist.append(os.path.join(root, file))
     else:
-        with open(args.file, "w") as f:
-            f.write(text)
+        raise ValueError("Invalid file path")
+
+    for file in filelist:
+        with open(file, "r") as f:
+            text = f.read()
+
+        for k, v in replace_map.items():
+            text = text.replace(k, v)
+
+        if args.dry:
+            print(text)
+        else:
+            with open(file, "w") as f:
+                f.write(text)
